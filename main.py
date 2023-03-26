ef16b996cbc61aea581467a0803363a581344653
ekate <ekaterina_kostrykina2006@gmail.com>
2023-03-26 18:25:16 +0300

    .

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+SCHOOL_NAME = 'МАОУ "Лицей 44"'
+
+
+@app.route('/')
+def main_page():
+    with open("static/documents/attention.txt", encoding='utf8') as f:
+        data = f.readlines()
+    at = ''
+    for i in data:
+        at += i.strip() + ' '
+    print(at)
+    return render_template('main_page.html',
+                           school_name=SCHOOL_NAME, attention=at)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
