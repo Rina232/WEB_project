b444c11ef1404dfe3ea519361aeccf36bfdda867
ekate <ekaterina_kostrykina2006@gmail.com>
2023-04-15 22:30:01 +0300

    Формы для опросов

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request
 import xlrd3
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['SECRET_KEY'] = 'lnu8b5v5ervdfgbhjnij987867c56erdg'
 
 SCHOOL_NAME = 'МАОУ "Лицей 44"'
 
@@ -74,88 +74,32 @@ def schedule_page():
                            school_name=SCHOOL_NAME, attention=at, schdl=schedule)
 
 
-@app.route('/poll')
-def poll_page():
-    return render_template('poll.html',
+@app.route('/polls')
+def polls_page():
+    return render_template('polls.html',
                            school_name=SCHOOL_NAME, attention=at, polls=polls)
 
 
-@app.route('/polls/<name>', methods=['POST', 'GET'])
-def poll_pages(name):
+@app.route('/poll/<name>', methods=['POST', 'GET'])
+def poll_page(name):
     doc = [i['document'] for i in polls if i['name'] == name][0]
+    questions = []
     try:
-        with open(doc, encoding='utf8') as f:
+        with open(f'static/documents/{doc}', encoding='utf8') as f:
             data = f.readlines()
+            for i in data:
+                questions.append(i.strip())
+        if len(questions) == 0:
+            raise Exception('Файл пустой')
         if request.method == 'GET':
-            return f'''<!doctype html>
-                        <html lang="en" xmlns="http://www.w3.org/1999/html">
-                        <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/
-                                            css/bootstrap.min.css"
-                                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjK
-                                            Hr8RbDVddVHyTfAAsrekwKmP1"
-                                            crossorigin="anonymous">
-                            <title>{SCHOOL_NAME}</title>
-                            <link rel="stylesheet"
-                                  href="{url_for('static', filename='css/style.css')}" type="text/css">
-                            <link rel="shortcut icon" href="static/img/logo.jpg" type="image/jpg">
-                        
-                        </head>
-                        <body>
-                            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha38
-                            4-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossori
-                            gin="anonymous"></script>
-                            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/poppe
-                            r.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakF
-                            PskvXusvfa0b4Q" crossorigin="anonymous"></script>
-                            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-                             integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76P
-                             VCmYl" crossorigin="anonymous"></script>
-                            <img src="/static/img/school.jpg" alt="Здесь должна быть картинка, но она не нашлась.">
-                            <div class="bottomleft">
-                                    <span style="padding: 0.5vw;">{SCHOOL_NAME}</span>
-                            </div>
-                            <marquee behavior="scroll" direction="left">{at}</marquee>
-                            <p style="margin-top: 4vw; margin-left: 7vw;"><a href="/">Главная</a>
-                            <a href="/poll"> / Опросы</a><a> / {name}</a></p>
-                            <section>
-                                <nav>
-                                    <form action="/">
-                                        <button>Новости и объявления</button>
-                                    </form>
-                                    <form action="/">
-                                        <button>Информация</button>
-                                    </form>
-                                    <form action="/schedule">
-                                        <button>Расписание</button>
-                                    </form>
-                                    <form action="/chess">
-                                        <button>Шахматы</button>
-                                    </form>
-                                    <form action="/poll">
-                                        <button>Опросы</button>
-                                    </form>
-                                </nav>
-                                <article>
-                                </article>
-                            </section>
-                            <footer>
-                                <h2 style="margin-top: 2vw; margin-left: 2vw; margin-bottom: 1.5vw;">КОНТАКТЫ</h2>
-                                <h3 style="margin-left: 2vw; margin-bottom: 1.5vw;"><b>МАОУ «ЛИЦЕЙ 44»
-                                 Г. ЛИПЕЦКА</b></h3>
-                                <p style="margin-left: 2vw; ">398050, Россия, Липецкая обл., г. Липецк, ул.
-                                 Плеханова, д. 51А, 49 <br> +7 (4742) 28-02-02 (приёмная), 27-04-63 <br>
-                                  sc44lip@schools48.ru</p>
-                            </footer>
-                        </body>
-                        </html>'''
-        if request.method == 'POST':
-            pass
+            return render_template('poll.html', school_name=SCHOOL_NAME, attention=at, quest=questions, name=name)
+        elif request.method == 'POST':
+            return render_template('conclusion.html', school_name=SCHOOL_NAME,
+                                   attention=at, con='Спасибо за прохождение опроса!')
     except Exception as e:
         print(e)
+        return render_template('conclusion.html', school_name=SCHOOL_NAME,
+                               attention=at, con='Произошли технические неполадки')
 
 
 if __name__ == '__main__':
